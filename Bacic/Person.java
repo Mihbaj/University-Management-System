79f3a4fa724230892fafaf58a2972869bc899f99
Mihbaj <mihbaj10@gmail.com>
2026-03-02 21:28:46 +0530

    UMS(University Management System)

---
+++ Bacic/Person.java
@@ -0,0 +1,28 @@
+package Bacic;
+abstract class Person{
+    private int id;
+    private String name;
+    private String email;
+    public Person(int id, String name, String email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String gtEmail(){
+        return email;
+    }
+
+    public  void DisplayInfo(){
+        System.out.println("ID: "+ id);
+        System.out.println("Name: "+ name);
+        System.out.println("Email: "+ email);
+    }
+
+
+}
